08bd4af3828b52fa2f7f9549652a461ee91f9938
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-18 01:36:39 +0900

    feat:TranslationExporter

---
+++ ExtremeSkins.Generator.Core/TranslationExporter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ExtremeSkins.Generator.Core;
+
+public sealed class TranslationExporter
+{
+    public string AmongUsPath
+    {
+        init
+        {
+            this.transPath = Path.Combine(value, @"CreatorMode/translation.csv");
+            this.configPath = Path.Combine(value, @"BepInEx/config/me.yukieiji.extremeskins.cfg");
+        }
+    }
+    private string transPath;
+    private string configPath;
+
+    public void AddTransData(Dictionary<string, string> exportData)
+    {
+
+    }
+}
